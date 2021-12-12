52162326dc14b89d6c636f1def33346f0d02818f
Steven Indzeoski <steven.indzeoski@gmail.com>
2021-12-12 11:33:43 -0600

    WIP day 8 part 2.

--- day8/part2.py
+++ day8/part2.py
@@ -1,8 +1,9 @@
-from typing import Deque
+# from typing import Deque
+import fileinput
 
 
-digits_by_segments = {5: {2, 3, 5}, 6: {0, 6, 9}}
-unique = {2: 1, 3: 7, 4: 4, 7: 8}
+# digits_by_segments = {5: {2, 3, 5}, 6: {0, 6, 9}}
+# unique = {2: 1, 3: 7, 4: 4, 7: 8}
 
 # Subsets
 # - 0: 1, 7
@@ -29,73 +30,35 @@ unique = {2: 1, 3: 7, 4: 4, 7: 8}
 # 9: 8
 
 
-def countDigitsWithUniqueSegments(outputs):
-    unique_amounts = {2, 3, 4, 7}
-    return len([i for i in outputs if len(i) in unique_amounts])
+# def countDigitsWithUniqueSegments(outputs):
+#     unique_amounts = {2, 3, 4, 7}
+#     return len([i for i in outputs if len(i) in unique_amounts])
 
 
-def parseOutputValues(filename):
-    outputs = []
+# def parseOutputValues(filename):
+#     outputs = []
 
-    with open(filename) as f:
-        for line in f:
-            output = line.split("|")[1].split()
-            outputs.extend(output)
+#     with open(filename) as f:
+#         for line in f:
+#             output = line.split("|")[1].split()
+#             outputs.extend(output)
 
-    return outputs
-
-
-def parseSignalPatterns(filename):
-    patterns = []
-
-    with open(filename) as f:
-        for line in f:
-            patterns_for_line = line.split("|")[0].split()
-            patterns.append(patterns_for_line)
-
-    return patterns
+#     return outputs
 
 
-def main():
-    patterns = parseSignalPatterns("sample1.txt")
+# def parseSignalPatterns(filename):
+#     patterns = []
 
-    for signal_patterns in patterns:
-        print(signal_patterns)
-        digits = {}
-        remaining_signals = Deque()
+#     with open(filename) as f:
+#         for line in f:
+#             patterns_for_line = line.split("|")[0].split()
+#             patterns.append(patterns_for_line)
 
-        for signal in signal_patterns:
-            length = len(signal)
-            if length in unique:
-                digits[unique[length]] = set(signal)
-            else:
-                remaining_signals.append(signal)
-
-        print(digits)
-        print(remaining_signals)
-
-        # for signal in remaining_signals:
-        while len(remaining_signals) > 0:
-            signal = remaining_signals.popleft()
-            length = len(signal)
-
-            if length == 5:
-                # 2, 3, 5
-                if digits[1].issubset(signal):
-                    # 3
-                    digits[3] = set(signal)
-                    continue
-            elif length == 6:
-                # 0, 6, 9
-                if digits[1].issubset(signal) and digits[7].issubset(signal):
-                    # 0
-                    digits[0] = set(signal)
-                    continue
-
-            remaining_signals.append(signal)
-
-        print(digits)
+#     return patterns
 
 
-if __name__ == "__main__":
-    main()
+for line in fileinput.input():
+    signals, output = line.strip().split(" | ")
+    # print("{} => {}".format(signals, output))
+    d = {l: set(s) for s in signals.split() if (l := len(s)) in {2, 4}}
+    print(d)
