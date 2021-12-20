04fee82e44c29ccede8001ce86d9b71466adb11f
Steven Indzeoski <steven.indzeoski@gmail.com>
2021-12-19 23:06:19 -0600

    Finished day 17 part 1.

---
+++ day17/part1.py
@@ -0,0 +1,63 @@
+import re
+from collections import namedtuple
+import fileinput
+import math
+
+Target = namedtuple("Target", ["x_min", "x_max", "y_min", "y_max"])
+
+
+class TooFarError(BaseException):
+    pass
+
+
+class Probe:
+    def __init__(self, vel_x: int, vel_y: int, target: Target) -> None:
+        self.x = 0
+        self.y = 0
+        self.vel_x = vel_x
+        self.vel_y = vel_y
+        self.target = target
+
+    def move(self) -> bool:
+        self.x += self.vel_x
+        self.y += self.vel_y
+        # print(self.x, self.y)
+        self.vel_x = self.vel_x - 1 if self.vel_x > 0 else 0
+        self.vel_y -= 1
+
+        if self.x > self.target.x_max or self.y < self.target.y_min:
+            raise TooFarError()
+
+        if self.x > self.target.x_min and self.y < self.target.y_max:
+            return True
+
+        return False
+
+
+def parse_file() -> Target:
+    match = re.search("x=(\d+)\.\.(\d+), y=(-*\d+)\.\.(-*\d+)", next(fileinput.input()))
+    return Target(int(match[1]), int(match[2]), int(match[3]), int(match[4]))
+
+
+target = parse_file()
+max_y = -math.inf
+
+for x in range(1, target.x_max + 1):
+    for y in range(target.y_min, target.x_max):
+        probe = Probe(x, y, target)
+        max_probe_y = -math.inf
+
+        try:
+            while True:
+                found = probe.move()
+                max_probe_y = max(max_probe_y, probe.y)
+
+                if found:
+                    max_y = max(max_y, max_probe_y)
+                    # print("Success!")
+                    break
+        except TooFarError:
+            # print("Went too far!")
+            pass
+
+print(max_y)
